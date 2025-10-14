f3e90bdaa393bfe63169dd32107b283807320202
mateus <mateusmacedoeng@gmail.com>
2025-10-14 05:38:08 -0300

    add handling errors in use cases

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 import { RegisterUseCase } from '@/use-cases/register'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -22,7 +23,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
             password,
         })
     } catch (error) {
-        return reply.status(409).send()
+        if (error instanceof UserAlreadyExistsError) {
+            return reply.status(409).send({ message: error.message })
+        }
+
+        return reply.status(500).send() //TODO: fix me later
     }
 
     return reply.status(201).send()
