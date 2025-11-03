6f7a882e6a769f02c21d077eeb21aa90687097c7
Mateus <mateusmacedoeng@gmail.com>
2025-11-03 20:51:25 -0300

    refactoring: create factory pattern for register and authenticate controller

--- src/use-cases/factories/make-register-use-case.ts
+++ src/use-cases/factories/make-register-use-case.ts
@@ -1,1 +1,9 @@
-export function makeRegisterUseCase() {}
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { RegisterUseCase } from '../register'
+
+export function makeRegisterUseCase() {
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    return registerUseCase
+}
