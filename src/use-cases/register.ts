26b5830278931078d21ef497d93b8c390b6629fa
Mateus <mateusmacedoeng@gmail.com>
2025-11-03 20:36:06 -0300

    fix bug in prismaUsersRepository constant

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,5 @@
 import { hash } from 'bcryptjs'
 import type { User } from 'generated/prisma'
-import { prisma } from '@/lib/prisma'
 import type { UsersRepository } from '@/repositories/users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
