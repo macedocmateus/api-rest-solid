ce816fa43f3c4be9f571928bb9db2bd5d747e207
Mateus <mateusmacedoeng@gmail.com>
2025-11-11 22:19:51 -0300

    add get user profile use-case and get user profile tests

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,12 @@
-import type { Prisma } from 'generated/prisma'
+import type { Prisma, User } from 'generated/prisma'
 import { prisma } from '@/lib/prisma'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+    findById(id: string): Promise<User | null> {
+        throw new Error('Method not implemented.')
+    }
+
     async findByEmail(email: string) {
         const user = await prisma.user.findUnique({
             where: {
